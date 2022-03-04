e4f3b303a2e48009577826c73fa6ba5a95f6d74d
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-03-04 20:58:39 +0500

    Add js code

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let numbers = [ 3.02, 3.65, 5, -9, ];
 
 let newArr = [];
 
-function checkNumber(Numbers) {
+function checkNumber(numbers) {
   for (let num of numbers) {
     if(num < 0 ){
       newArr.push(Math.round(Math.abs(num ) ) );
