7a0b6843e44bdd6542b0b1429737e2986d93981b
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-03-04 20:00:15 +0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let numbers = [ 3.02, 3.65, 5, -9, ];
+
+let newArr = [];
+
+function checkNumber(Numbers) {
+  for (let num of numbers) {
+    if(num < 0 ){
+      newArr.push(Math.round(Math.abs(num ) ) );
+    }else{
+      newArr.push(Math.round(num ) );
+    }
+  }
+  return newArr
+}
+
+console.log(checkNumber(numbers));
